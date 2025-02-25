c435834994af7e7011aaa19765e80e2de33d9743
DiegoMB3254 <diegoamunoz2005@gmail.com>
2025-02-25 17:09:19 -0500

    agregar modelos

---
+++ src/main/java/Modelo/Usuario.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author MI PC
+ */
+public class Usuario {
+    private String nombre;
+    private String email;
+    private String contraseña;
+    private String tipoUsuario;
+
+    public Usuario(String nombre, String email, String contraseña, String tipoUsuario) {
+        this.nombre = nombre;
+        this.email = email;
+        this.contraseña = contraseña;
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+}
