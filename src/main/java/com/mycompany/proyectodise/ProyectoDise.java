089059a64182b2ec3ef0bde15fd859327c0b02db
Fate <andresfeli107911@gmail.com>
2025-02-25 15:58:19 -0500

    comienzo del proyecto

---
+++ src/main/java/com/mycompany/proyectodise/ProyectoDise.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectodise;
+
+import java.net.URL;
+
+/**
+ *
+ * @author ANDRES FELIPE
+ */
+public class ProyectoDise {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+  
+}
