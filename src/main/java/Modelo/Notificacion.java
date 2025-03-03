4492088e7af55e18a422d7713e1f5fe4830f4fac
DiegoMB3254 <diegoamunoz2005@gmail.com>
2025-03-03 18:37:55 -0500

    agregado modelo de notificacion

--- src/main/java/Modelo/Notificacion.java
+++ src/main/java/Modelo/Notificacion.java
@@ -4,10 +4,43 @@
  */
 package Modelo;
 
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
 /**
  *
  * @author MI PC
  */
-public class Notificacion {
-    
+public class Notificacion implements Serializable {
+
+    private String mensaje;
+    private LocalDateTime fechaHora;
+    private boolean leida;
+
+    public Notificacion(String mensaje) {
+        this.mensaje = mensaje;
+        this.fechaHora = LocalDateTime.now();
+        this.leida = false;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public boolean isLeida() {
+        return leida;
+    }
+
+    public void marcarComoLeida() {
+        this.leida = true;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + fechaHora + "] " + mensaje + (leida ? " (Leída)" : " (No leída)");
+    }
 }
